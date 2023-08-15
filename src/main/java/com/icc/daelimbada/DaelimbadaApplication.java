1804d8a3618b5fc5dbd8cd9021e17456777e0929
JunSeong_Park <ttotto0608@naver.com>
2023-08-16 00:12:08 +0900

    feat : adding base time entity

--- src/main/java/com/icc/daelimbada/DaelimbadaApplication.java
+++ src/main/java/com/icc/daelimbada/DaelimbadaApplication.java
@@ -2,7 +2,9 @@ package com.icc.daelimbada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DaelimbadaApplication {
 
--- src/main/java/com/icc/daelimbada/common/domain/BaseTime.java
+++ src/main/java/com/icc/daelimbada/common/domain/BaseTime.java
@@ -1,4 +1,26 @@
 package com.icc.daelimbada.common.domain;
 
-public class BaseTime {
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+    @CreatedDate
+    @Column(updatable = false, name = "regdate")
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(updatable = true, name = "moddate")
+    private LocalDateTime modDate;
 }
+
