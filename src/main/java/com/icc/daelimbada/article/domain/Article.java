c3ba8939bbbc01ff82bccf7a1d860152df9b4fb4
JunSeong_Park <ttotto0608@naver.com>
2023-08-16 11:04:29 +0900

    refactor : article entity 1

--- src/main/java/com/icc/daelimbada/article/domain/Article.java
+++ src/main/java/com/icc/daelimbada/article/domain/Article.java
@@ -1,16 +1,18 @@
 package com.icc.daelimbada.article.domain;
 
 import com.icc.daelimbada.common.domain.BaseTime;
+import com.icc.daelimbada.common.domain.BooleanToYNConverter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+/**
+ * img 를 2개 이상 사용하려면 해당 ERD 구조로는 해겳 불가능
+ * major code 를 사용하려는 취지는 좋으나 불필요한 쿼리 연산을 또 실행하는 과정이 과연 효율적인지 의문
+ */
 @Data
 @Entity
 @Builder
@@ -20,4 +22,17 @@ public class Article extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 100)
+    private String title;
+
+    @Column
+    private Long price;
+
+    @Column(length = 1000)
+    private String content;
+
+    @Column
+    @Convert(converter = BooleanToYNConverter.class)
+    private boolean isSold;
 }
