bad3a27016c8b8e68768fbce4de0bbce0f150ec7
JunSeong_Park <ttotto0608@naver.com>
2023-08-28 16:25:26 +0900

    feat : adding controller user, article, image, reply

---
+++ src/main/java/com/icc/daelimbada/reply/controller/ReplyController.java
@@ -0,0 +1,13 @@
+package com.icc.daelimbada.reply.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/reply")
+public class ReplyController {
+}
