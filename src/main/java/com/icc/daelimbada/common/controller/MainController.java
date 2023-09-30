d7ff9a8981a766afaab07f575649aceea437c86f
JunSeong_Park <ttotto0608@naver.com>
2023-09-30 15:55:41 +0900

    refactor : change username to email for login

--- src/main/java/com/icc/daelimbada/common/controller/MainController.java
+++
@@ -1,9 +0,0 @@
-package com.icc.daelimbada.common.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class MainController {
-
-}
--- src/main/java/com/icc/daelimbada/user/dto/LoginDTO.java
+++ src/main/java/com/icc/daelimbada/user/dto/LoginDTO.java
@@ -6,6 +6,6 @@ import lombok.Data;
 @Data
 @Builder
 public class LoginDTO {
-    private String username;
+    private String email;
     private String password;
 }
